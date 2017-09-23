e59fe97af73b10f4d07efde98aee200318f8ca38
John Johnson <johnrjj@gmail.com>
2017-09-23 13:38:38 -0400

    Initial commit

---
+++ src/tutorial/1.ts
@@ -0,0 +1,42 @@
+import * as GTT from 'gdax-trading-toolkit';
+import { GDAXFeed } from "gdax-trading-toolkit/build/src/exchanges";
+import { OrderbookMessage } from "gdax-trading-toolkit/build/src/core";
+
+const logger = GTT.utils.ConsoleLoggerFactory();
+const products: string[] = ['BTC-USD', 'ETH-USD', 'LTC-USD'];
+const tallies: any = {};
+products.forEach((product: string) => {
+    tallies[product] = {};
+});
+
+let count = 0;
+
+GTT.Factories.GDAX.FeedFactory(logger, products).then((feed: GDAXFeed) => {
+    feed.on('data', (msg: OrderbookMessage) => {
+        count++;
+        if (!(msg as any).productId) {
+            tallies.other += 1;
+        } else {
+            const tally = tallies[msg.productId];
+            if (!tally[msg.type]) {
+                tally[msg.type] = 0;
+            }
+            tally[msg.type] += 1;
+        }
+        if (count % 1000 === 0) {
+            printTallies();
+        }
+    });
+}).catch((err: Error) => {
+    logger.log('error', err.message);
+    process.exit(1);
+});
+
+function printTallies() {
+    console.log(`${count} messages received`);
+    for (const p in tallies) {
+        const types = Object.keys(tallies[p]).sort();
+        const tally: string = types.map((t) => `${t}: ${tallies[p][t]}`).join('\t');
+        console.log(`${p}: ${tally}`);
+    }
+}
